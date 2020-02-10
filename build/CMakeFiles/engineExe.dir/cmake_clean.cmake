b6b2f49d7e9a6de108180aabdbaa31a42eba9ea6
Abdou <afifiabdou@gmail.com>
2020-02-11 00:43:54 +0100

    a simple cmake conf

---
+++ build/CMakeFiles/engineExe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engineExe.dir/src/main.cpp.o"
+  "CMakeFiles/engineExe.dir/src/hello.cpp.o"
+  "engineExe.pdb"
+  "engineExe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engineExe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
